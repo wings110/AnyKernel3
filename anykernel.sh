d41bce3fbf703d74edcec7a6009d422894d284c5
wings110 <283982871@qq.com>
2024-11-03 12:54:23 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Constructed by linying2024 @ github (Kernel by wu17481748 @ github)
+kernel.string=ExampleKernel by osm0sis @ xda-developers （kernel by wings110）
 do.devicecheck=1
 do.modules=0
 do.systemless=1
